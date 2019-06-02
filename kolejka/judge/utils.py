b67bd18e8cd1aac2a7c6ec16d32f56cd393d1257
Tomasz Zieliński <zielinskit@users.noreply.github.com>
2019-06-02 23:18:04 +0200

    Update documentation with examples

--- kolejka/judge/utils.py
+++ kolejka/judge/utils.py
@@ -1,28 +1,12 @@
 import argparse
-import json
 
-from kolejka.judge.environments import LocalComputer, KolejkaObserver, PsutilEnvironment, KolejkaTask
+from kolejka.judge.environments import LocalComputer, KolejkaObserver, PsutilEnvironment
 
 KNOWN_ENVIRONMENTS = {
     'local': LocalComputer,
     'kolejkaobserver': KolejkaObserver,
     'psutil': PsutilEnvironment,
-    'kolejkatask': KolejkaTask,
 }
-
-
-def parse_kolejkatask_arguments(command):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--output_dir', dest='output_directory')
-    parser.add_argument('--image', dest='image')
-    parser.add_argument('--files', dest='files', nargs='*')
-    parser.add_argument('--environment', dest='environment', type=json.loads)
-    parser.add_argument('--requires', dest='requires')
-    parser.add_argument('--exclusive', dest='exclusive')
-    parser.add_argument('--limits', dest='limits')
-    parser.add_argument('--collect', dest='collect')
-    parser.add_argument('--config', dest='config')
-    return vars(parser.parse_known_args(command)[0])
 
 
 def parse_default_arguments(command):
@@ -35,7 +19,6 @@ KNOWN_ENVIRONMENTS_ARGUMENTS = {
     'local': parse_default_arguments,
     'kolejkaobserver': parse_default_arguments,
     'psutil': parse_default_arguments,
-    'kolejkatask': parse_kolejkatask_arguments,
 }
 
 
