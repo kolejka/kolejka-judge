ada0fe8f97f125e2f496853f41e238c221366cfa
Tomasz Zieliński <zielinskit@users.noreply.github.com>
2019-06-24 22:08:27 +0200

    Fix typo in import

--- tests/integrations/auto_compile/run.py
+++ tests/integrations/auto_compile/run.py
@@ -4,12 +4,12 @@ from kolejka.judge.checking import Checking
 from kolejka.judge.commands.check import RunChecker
 from kolejka.judge.commands.compile.java import CompileJava
 from kolejka.judge.commands.run.java import RunJavaClassSolution
-from kolejka.judge.tasks.autocompile import AutoCompileTask
+from kolejka.judge.tasks.autocompile import AutoCompile
 from kolejka.judge.utils import detect_environment
 
 checking = Checking(environment=detect_environment())
 checking.add_steps(
-    compile_checker=AutoCompileTask('checker.cpp'),
+    compile_checker=AutoCompile('checker.cpp'),
     compile_solution=CompileJava('Solution.java'),
     solution=RunJavaClassSolution(class_name='Solution', stdout='out'),
     check=RunChecker('autocompile/run.sh', stdin='out'),
