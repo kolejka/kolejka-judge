f33c22e5795f7261a6c8d257ceb4a0dbd7fa7d2b
Grzegorz Gutowski <grzegorz.gutowski@tcs.uj.edu.pl>
2020-04-29 23:20:06 +0200

    Runnable wheel magic

--- packages/wheel_repair.py
+++ packages/wheel_repair.py
@@ -4,6 +4,7 @@
 
 import argparse
 from pathlib import Path
+import shutil
 import subprocess
 import tempfile
 
@@ -17,4 +18,18 @@ for wheel in args.wheel:
     assert wheel.is_file()
     with tempfile.TemporaryDirectory() as work_dir:
         subprocess.run(['unzip', str(wheel)], cwd=work_dir, check=True)
-        subprocess.run(['zip', '-9', str(wheel), '-r', '.'], cwd=work_dir, check=True)
+        with ( Path(work_dir) / '__main__.py' ).open('w') as main_file:
+            main_file.write('''
+if __name__ == '__main__':
+    from kolejka.judge import main
+    main()
+''')
+        with tempfile.TemporaryDirectory() as zip_dir:
+            first = Path(zip_dir) / 'first.zip'
+            subprocess.run(['zip', '-9', str(first), '-r', '.'], cwd=work_dir, check=True)
+            second = Path(zip_dir) / 'second.zip'
+            with second.open('wb') as second_file:
+                second_file.write(bytes('#!/usr/bin/env python3\n', 'utf8'))
+                with first.open('rb') as first_file:
+                    second_file.write(first_file.read())
+            shutil.move(second, wheel)
