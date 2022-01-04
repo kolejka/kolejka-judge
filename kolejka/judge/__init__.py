299758a46ee1f8c75f3d967c32a4efc82a3b67b8
Grzegorz Gutowski <grzegorz.gutowski@uj.edu.pl>
2022-01-04 11:29:20 +0100

    Do not depend on setproctitle to be installed.

--- kolejka/judge/__init__.py
+++ kolejka/judge/__init__.py
@@ -4,10 +4,14 @@
 def main(judge_path=None):
     import argparse
     import logging
-    import setproctitle
     from kolejka.judge.judge import config_parser as judge_parser
 
-    setproctitle.setproctitle('kolejka-judge')
+    try:
+        import setproctitle
+        setproctitle.setproctitle('kolejka-judge')
+    except:
+        pass
+
     parser = argparse.ArgumentParser(description='KOLEJKA judge')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='show more info')
     parser.add_argument('-d', '--debug', action='store_true', default=False, help='show debug info')
