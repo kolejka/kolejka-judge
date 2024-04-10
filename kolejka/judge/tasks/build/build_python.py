cba10424df22537d6410efd4a84babbcda426a26
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2024-04-10 09:54:45 +0200

    venvs are only created if needed

--- kolejka/judge/tasks/build/build_python.py
+++ kolejka/judge/tasks/build/build_python.py
@@ -28,24 +28,31 @@ class BuildPython3ScriptTask(BuildScriptTask):
     @default_kwargs
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.venv_required = False
+
+    def get_wheel_files(self):
+        wheel_globs = ['*.[Ww][Hh][Ll]']
+        
+        return self.get_source_files(wheel_globs)
 
     def get_execution_commands(self):
         base_commands = super().get_execution_commands()
-        #if exists:
-        env_commands = [".", self.build_directory/"venv/bin/activate"]
         
-        result = [env_commands] + base_commands
-
-        return result
+        if not self.venv_required:
+            return base_commands
+        
+        env_commands = [".", self.build_directory/f"{config.PYTHON_VENV_NAME}/bin/activate"]
+        return [env_commands] + base_commands
 
     def execute_build(self):
         #self.run_command('install-numpy', InstallPackageIntoVenv, venv="shared/env", package="tqdm")
-        
-        wheel_globs = ['*.[Ww][Hh][Ll]']
-        wheels = self.get_source_files(wheel_globs)
-
+               
+        wheels = self.get_wheel_files()
+                
         if wheels:
             self.run_command('venv', CreateVenvCommand, path=self.build_directory/"venv")
+            self.venv_required = True
+                
         for wheel in wheels:
             semantic_part = str(wheel).split('/')[-1]
             print("Installing wheel", semantic_part)
